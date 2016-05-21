7635ca7b0000c7c265d6e568407237aa1c655190
Deepak Ladha <ladha.dee@gmail.com>
2016-05-21 17:22:39 +0530

    Corrected comments for plot 4

--- plot4.R
+++ plot4.R
@@ -15,13 +15,13 @@ with(elec_data, {
     # Plot 1 (1,1)
     plot(Time, Global_active_power, xlab = "", ylab = "Global Active Power", type = "l")
     
-    # Plot 2 (1,2)
+    # Plot 2 (2,1)
     plot(Time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
     lines(Time, Sub_metering_2, type = "l", col = "red")
     lines(Time, Sub_metering_3, type = "l", col = "blue")
     legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
     
-    # Plot 3 (2,1)
+    # Plot 3 (1,2)
     plot(Time, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
     
     #plot 4 (2,2)
